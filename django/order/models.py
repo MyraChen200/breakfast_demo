98f6f76a081a66de3591219269ab680489a471ac
MyraChen <myra@thetigerparty.com>
2019-07-13 16:38:40 +0800

    Add model and requirement doc

--- django/order/models.py
+++ django/order/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class order(models.Model):
+    order_id = models.IntegerField(null=False, primary_key=True)
+    customer_id = models.IntegerField(null=False)
+    shipping = models.IntegerField(null=False)
+    created_at = models.CharField(max_length=255, null=False, blank=False)
+
+class order_item(models.Model):
+    order_id = models.ForeignKey('order', null=False, blank=False, db_column='order_id', on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=255, null=False, blank=False)
+    qty = models.IntegerField(null=False)
 
